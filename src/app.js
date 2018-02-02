a1a24e8c12e1290de6a60ecc2cd8dc5ab8d2f9e1
Joe Sweeney <joe.sweeney224@gmail.com>
2018-02-01 22:20:36 -0700

    better map tiles

--- src/app.js
+++ src/app.js
@@ -16,9 +16,12 @@ L.Icon.Default.mergeOptions({
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+
+Esri_WorldImagery.addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
